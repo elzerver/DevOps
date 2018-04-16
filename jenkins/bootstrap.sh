063d2ffe079eb4d9c704136bf176f7d2a39d9da2
Aldo Rivadeneira <aldo.rivadeneira@afirma.solutions>
2018-04-16 13:32:53 -0500

    adding the commands to setup with the bootstrap script

--- jenkins/bootstrap.sh
+++ jenkins/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 vagrant up
-ssh-keygen -f ~/.ssh/known_hosts -R example-dotcom
-ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i ./hosts playbook.yml
+#ssh-keygen -f ~/.ssh/known_hosts -R example-dotcom
+#ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i ./hosts playbook.yml
+ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i ./hosts jenkins-master.yml
+ansible-playbook --private-key=~/.vagrant.d/insecure_private_key -u vagrant -i ./hosts jenkins-slave.yml
+
+
