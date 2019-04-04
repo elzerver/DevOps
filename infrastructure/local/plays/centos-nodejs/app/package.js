95a4c74628310c1d207de0b4e38b94569f3452cc
Aldo <aldo.rivadeneira@gmail.com>
2019-04-04 11:35:49 -0600

    infrastructure
    test  package

---
+++ infrastructure/local/plays/centos-nodejs/app/package.js
@@ -0,0 +1,9 @@
+{
+    "name": "exampledemoapp",
+    "description": "Example Express Node.js app.",
+    "author": "XXX",
+    "dependencies": {
+        "express": "3.x.x"
+    },
+    "engine": "node >=0.10.6"
+}
